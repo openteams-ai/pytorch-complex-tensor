feee909bb82533903a7cfb329cb0cf2640d53879
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2025-08-06 07:55:42 +0200

    Move and rename `TestInfo`.

--- src/complex_tensor/test/test_ops.py
+++ src/complex_tensor/test/test_ops.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
 
-import dataclasses
-from dataclasses import dataclass, field
-
 import torch
 from torch._ops import OpOverload
 from torch.testing._internal.common_device_type import instantiate_device_type_tests, ops
@@ -12,6 +9,8 @@ from torch.testing._internal.opinfo.core import OpInfo
 
 from complex_tensor import ComplexTensor
 from complex_tensor.ops.core import COMPLEX_OPS_TABLE, ORDERED_OPS_LIST
+
+from .utils import TestDescriptor
 
 torch._dynamo.config.recompile_limit = float("inf")
 
@@ -38,50 +37,28 @@ implemented_op_db = tuple(filter(lambda op: op.name in implemented_op_names, com
 ordered_op_db = tuple(filter(lambda op: op.name in ordered_op_names, op_db))
 
 
-@dataclass(frozen=True)
-class TestInfo:
-    op_name: str | None = field(default=None)
-    device: str | None = field(default=None)
-    dtype: torch.dtype | None = field(default=None)
-    compile: bool | None = field(default=None)
-
-    def matches(self, other: TestInfo) -> bool:
-        fields1 = dataclasses.fields(self)
-        fields2 = dataclasses.fields(other)
-        if fields1 != fields2:
-            return False
-
-        for f in fields1:
-            f1 = getattr(self, f.name)
-            f2 = getattr(other, f.name)
-            if f1 is not None and f2 is not None and f1 != f2:
-                return False
-
-        return True
-
-
 SKIPS = {
-    TestInfo(op_name="real"): "`aten.real` does not hit `__torch_dispatch__`",
-    TestInfo(op_name="imag"): "`aten.imag` does not hit `__torch_dispatch__`",
-    TestInfo(
+    TestDescriptor(op_name="real"): "`aten.real` does not hit `__torch_dispatch__`",
+    TestDescriptor(op_name="imag"): "`aten.imag` does not hit `__torch_dispatch__`",
+    TestDescriptor(
         op_name="sub", dtype=torch.complex32, compile=True
     ): "numerical precision optimized out",
-    TestInfo(
+    TestDescriptor(
         op_name="sort"
     ): "https://github.com/pytorch/pytorch/pull/159556#issuecomment-3154215299",
-    TestInfo(
+    TestDescriptor(
         op_name="minimum"
     ): "https://github.com/pytorch/pytorch/pull/159556#issuecomment-3154215299",
-    TestInfo(
+    TestDescriptor(
         op_name="maximum"
     ): "https://github.com/pytorch/pytorch/pull/159556#issuecomment-3154215299",
-    TestInfo(
+    TestDescriptor(
         op_name="argmin"
     ): "https://github.com/pytorch/pytorch/pull/159556#issuecomment-3154215299",
-    TestInfo(
+    TestDescriptor(
         op_name="argmax"
     ): "https://github.com/pytorch/pytorch/pull/159556#issuecomment-3154215299",
-    TestInfo(
+    TestDescriptor(
         op_name="topk"
     ): "https://github.com/pytorch/pytorch/pull/159556#issuecomment-3154215299",
 }
@@ -101,9 +78,9 @@ class TestComplexTensor(TestCase):
     _default_dtype_check_enabled = True
 
     @parametrize("compile", [False, True])
-    @ops(implemented_op_db, allowed_dtypes=complex_types)
+    @ops(implemented_op_db, allowed_dtypes=list(complex_types))
     def test_consistency(self, device, dtype, op: OpInfo, compile: bool):
-        test_info = TestInfo(op_name=op.name, device=device, dtype=dtype, compile=compile)
+        test_info = TestDescriptor(op_name=op.name, device=device, dtype=dtype, compile=compile)
         for xfail_info, reason in SKIPS.items():
             if xfail_info.matches(test_info):
                 self.skipTest(reason)
@@ -130,7 +107,7 @@ class TestComplexTensor(TestCase):
 
     @ops(ordered_op_db, dtypes=list(complex_types))
     def test_ordered_raises(self, device, dtype, op: OpInfo):
-        test_info = TestInfo(op_name=op.name, device=device, dtype=dtype)
+        test_info = TestDescriptor(op_name=op.name, device=device, dtype=dtype)
         for xfail_info, reason in SKIPS.items():
             if xfail_info.matches(test_info):
                 self.skipTest(reason)
