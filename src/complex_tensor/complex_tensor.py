Fix a few precision issues.

src/complex_tensor/complex_tensor.py
@@ -89,6 +89,7 @@ class ComplexTensor(torch.Tensor):
         cls, func: OpOverload, types: tuple[type], args: tuple = (), kwargs: dict | None = None
     ):
         from .ops import lookup_complex
+        from .ops._common import DEBUG_SET
 
         kwargs = {} if kwargs is None else kwargs
 
@@ -96,7 +97,30 @@ class ComplexTensor(torch.Tensor):
         if impl is None:
             return NotImplemented
 
-        return impl(*args, **kwargs)
+        ret = impl(*args, **kwargs)
+
+        debug_set = DEBUG_SET.get()
+        if debug_set is not None and all(
+            disallowed_name not in str(func) for disallowed_name in ("empty", "rand")
+        ):
+            from torch.utils._pytree import tree_flatten, tree_map
+
+            from .ops._common import _as_interleaved
+
+            args_ref, kwargs_ref = tree_map(_as_interleaved, (args, kwargs))
+            ret_ref = func(*args_ref, **kwargs_ref)
+
+            ret_flat, _ = tree_flatten(ret)
+            ret_ref_flat, _ = tree_flatten(ret_ref)
+            if not all(
+                torch.allclose(_as_interleaved(r), rr, equal_nan=True)
+                for r, rr in zip(ret_flat, ret_ref_flat, strict=True)
+                if isinstance(rr, torch.Tensor)
+            ):
+                print((args, kwargs, ret, ret_ref))
+                debug_set.add(func)
+
+        return ret
 
     __torch_function__ = torch._C._disabled_torch_function_impl
 

src/complex_tensor/ops/_common.py
@@ -4,8 +4,8 @@ from typing import Any
 
 import torch
 from torch._decomp import get_decompositions
-from torch._ops import OpOverloadPacket
-from torch._refs import is_complex
+from torch._ops import OpOverload, OpOverloadPacket
+from torch._refs import is_complex as _is_complex
 from torch.utils._python_dispatch import TorchDispatchMode
 from torch.utils._pytree import tree_flatten, tree_map, tree_unflatten
 
@@ -96,15 +96,16 @@ DEBUG_SET: ContextVar[set[OpType] | None] = ContextVar("DEBUG_SET", default=None
 
 
 def lookup_complex(func: OpType, *args, **kwargs) -> Callable:
-    debug_set = DEBUG_SET.get()
-    if debug_set is not None:
-        debug_set.add(func)
     return COMPLEX_OPS_TABLE.get(
         func,
         COMPLEX_OPS_TABLE.get(
             func.overloadpacket, DECOMPOSITIONS.get(func, DECOMPOSITIONS.get(func.overloadpacket))
         ),
     )
+
+
+def is_complex(x: torch.Tensor, /) -> bool:
+    return not isinstance(x, torch.Tensor) and isinstance(x, complex) or _is_complex(x)
 
 
 def split_complex_arg(
@@ -218,7 +219,13 @@ class ComplexDispatchMode(TorchDispatchMode):
         self._compile = _compile
         self._debug = _debug
 
-    def __torch_dispatch__(self, func, types, args=(), kwargs=None):
+    def __torch_dispatch__(
+        self,
+        func: OpOverload,
+        types: tuple[type],
+        args: tuple = (),
+        kwargs: dict[str, Any] | None = None,
+    ):
         if kwargs is None:
             kwargs = {}
 

src/complex_tensor/ops/aten.py
@@ -10,6 +10,7 @@ from ._common import (
     ERROR_TYPES,
     OpType,
     complex_to_real_dtype,
+    is_complex,
     promote_real_cpu_tensors,
     register_binary_nonlinear,
     register_complex,
@@ -150,15 +151,19 @@ fill__impl = register_binary_linear(aten.fill_)
 @register_complex(aten.div)
 @register_complex(aten.true_divide)
 def div_impl(lhs: ComplexTensor, rhs: ComplexTensor, *, rounding_mode=None):
+    if rounding_mode is not None:
+        raise NotImplementedError
     a_r, a_i = split_complex_tensor(lhs)
+    if not is_complex(rhs):
+        return ComplexTensor(a_r / rhs, a_i / rhs)
     b_r, b_i = split_complex_arg(rhs)
     out_dt, (a_r, a_i, b_r, b_i) = promote_real_cpu_tensors(a_r, a_i, b_r, b_i)
     num_r = a_r * b_r + a_i * b_i
     num_i = a_i * b_r - a_r * b_i
     den = b_r * b_r + b_i * b_i
     return ComplexTensor(
-        aten.div(num_r, den, rounding_mode=rounding_mode).to(out_dt),
-        aten.div(num_i, den, rounding_mode=rounding_mode).to(out_dt),
+        (num_r / den).to(out_dt),
+        (num_i / den).to(out_dt),
     )
 
 
@@ -210,9 +215,11 @@ def abs_impl(self: ComplexTensor) -> torch.Tensor:
     x, y = split_complex_tensor(self)
     out_dt, (x, y) = promote_real_cpu_tensors(x, y)
     scale = torch.maximum(torch.abs(x), torch.abs(y))
+    x_scaled = x / scale
+    y_scaled = y / scale
     result = torch.where(
         scale.to(torch.bool),
-        torch.sqrt(torch.pow(x / scale, 2) + torch.pow(y / scale, 2)) * scale,
+        torch.sqrt(x_scaled * x_scaled + y_scaled * y_scaled) * scale,
         False,
     )
     return result.to(out_dt)
@@ -569,10 +576,8 @@ def sgn_impl(self: ComplexTensor) -> ComplexTensor:
     self_r, self_i = split_complex_tensor(self)
     out_dt, (self_r, self_i) = promote_real_cpu_tensors(self_r, self_i)
     abs_self = torch.abs(ComplexTensor(self_r, self_i))
-    mask = abs_self != 0
-    masked_sgn = ComplexTensor(
-        torch.div(self_r, abs_self).to(out_dt), torch.div(self_i, abs_self).to(out_dt)
-    )
+    mask = (self_r != 0) | (self_i != 0)
+    masked_sgn = ComplexTensor((self_r / abs_self).to(out_dt), (self_i / abs_self).to(out_dt))
     return torch.where(mask, masked_sgn, 0)
 
 

src/complex_tensor/test/test_ops.py
@@ -150,8 +150,7 @@ class TestComplexBwdGradients(TestGradients):
         if dtype not in op.supported_backward_dtypes(torch.device(device).type):
             self.skipTest(f"Skipped! {dtype=} is not in supported backward dtypes!")
 
-        with ComplexDispatchMode(_debug=True):
-            op.gradcheck_fast_mode = False
+        with ComplexDispatchMode():
             self._grad_test_helper(device, dtype, op, op.get_op())
 
 